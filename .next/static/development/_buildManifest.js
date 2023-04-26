806213273956edad7f964b31408d0fc8527f30c2
Jeff Masterson <j.mastersonjr@gmail.com>
2023-04-25 21:35:34 -0400

    test

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/websites":["static\u002Fchunks\u002Fpages\u002Fwebsites.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fwebsites"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
